3866accd4e2fde4505595242d88066af2558035a
frodo <eugene@moscow.ctm.ru>
2015-10-05 09:51:20 +0300

    fix warnings; code restyle

--- lab1.c
+++ lab1.c
@@ -5,49 +5,57 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 
-void main()
+int main(int argc, char ** argv)
 {
-pid_t pid;
-FILE *mf;
+	pid_t pid;
+	FILE *mf;
 
-char filename[20];
+	char filename[20];
 
-switch(pid=fork())
-{
-case -1: /*Ошибка*/
-	perror("fork");
-	exit(1);
+	switch(pid=fork())
+	{
+		case -1: /*Ошибка*/
+				perror("fork");
+				exit(1);
 
-case 0: /*Дочерний процесс*/                                        sleep(1);
-	printf("\nCHILD: Это дочерний процесс\n");                sleep(1);
-	printf("CHILD: PID: %d\n", getpid());                       sleep(1);
-	printf("CHILD: PPID: %d\n", getppid());                   sleep(1);
+		case 0: /*Дочерний процесс*/                                        
+				sleep(1);
+				printf("\nCHILD: Это дочерний процесс\n");                
+				sleep(1);
+				printf("CHILD: PID: %d\n", getpid());                       
+				sleep(1);
+				printf("CHILD: PPID: %d\n", getppid());                   
+				sleep(1);
 
-	sprintf(filename,"file_%d.txt", getpid());
-	mf=fopen(filename, "w");
-	fprintf(mf, "%d", getppid());
-	fclose(mf);
+				sprintf(filename,"file_%d.txt", getpid());
+				mf=fopen(filename, "w");
+				fprintf(mf, "%d", getppid());
+				fclose(mf);
 
-	printf("CHILD: Файл создан\n");
+				printf("CHILD: Файл создан\n");
 
-	exit(0);
+				exit(0);
 
-default: /*Родительский процесс*/
-	wait(0);
-	printf("\nPARENT: Это Родительский процесс\n");           sleep(1);
-	printf("PARENT: PID: %d\n", getpid());                      sleep(1);
-	printf("PARENT: PID потомка: %d\n", pid);                   sleep(1);
+		default: /*Родительский процесс*/
+				wait(0);
+				printf("\nPARENT: Это Родительский процесс\n");           
+				sleep(1);
+				printf("PARENT: PID: %d\n", getpid());                      
+				sleep(1);
+				printf("PARENT: PID потомка: %d\n", pid);                   
+				sleep(1);
 
-        sprintf(filename,"file_%d.txt", getpid());
-	mf=fopen(filename, "w");
-	fprintf(mf, "%d\n%d", getpid(), pid);
-	fclose(mf);
+				sprintf(filename,"file_%d.txt", getpid());
+				mf=fopen(filename, "w");
+				fprintf(mf, "%d\n%d", getpid(), pid);
+				fclose(mf);
 
-	printf("PARENT: Файл создан\n");
+				printf("PARENT: Файл создан\n");
 
-	/*printf("PARENT: Ожидание завершения процесса-потомка\n");   sleep(1);*/
+			/*printf("PARENT: Ожидание завершения процесса-потомка\n");   sleep(1);*/
 
-	printf("PARENT: Выход!\n\n");                               sleep(1);
-	exit(0);
-}
+				printf("PARENT: Выход!\n\n");                               
+				sleep(1);
+				exit(0);
+	}
 }
